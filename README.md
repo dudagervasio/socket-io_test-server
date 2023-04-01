851b91b37b350baa6c9922db00bf02936b13fda5
Duda Gervásio <dudagervasio@yahoo.com.br>
2023-03-31 21:46:16 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+"# socket-io_test-server" 
---
+++ server.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+
+const PORT = 3303;
+
+const express = require('express');
+
+const webApp = express();
+
+const http = require('http').Server( webApp );
+
+const cors = require('cors');
+
+const jwt = require('jsonwebtoken');
+
+const io = require('socket.io')( http, {
+	cors: {
+		origin: '*',
+	},
+	pingTimeout: 500,
+	maxHttpBufferSize: 1e7
+});;
+
+webApp.use( cors() );
+
+http.listen(PORT, function(){
+
+	console.log('listening on *:' + PORT);
+});
+
+io.use(function(socket, next){
+
+	console.log('Middleware');
+
+	if (socket.handshake.auth && socket.handshake.auth.token){
+
+		const { token } = socket.handshake.auth;
+
+		console.log('Middleware: Handshake');
+
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+
+			if (err) return next( new Error('Auth error'));
+
+			socket.isAuth = true;
+			socket.token = token;
+			socket.decodedToken = decoded;
+
+			next();
+
+	  	});
+	
+	}else{
+	
+		return next( new Error('Auth error'));
+	}    
+
+ });
+  
+ io.on('connect', (socket)=> {
+
+	console.log(`Socket Connected: ${socket.id}`)
+
+	socket.onAny( (eventName, ...args) => {
+
+		console.log(`${eventName}: ${args}`);
+
+		//console.log( socket.decodedToken );
+
+	})
+
+})
